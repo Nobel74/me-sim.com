4b4821e618992a4d4266a0176c69e0d134c88638
Francisco Fernández <paxfer@gmail.com>
2026-07-31 14:25:08 +0200

    marketing: integrados Google Analytics, Tag Manager y meta de Search Console

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css';
 import { Barlow, Barlow_Condensed, Barlow_Semi_Condensed } from 'next/font/google';
 import ClientLayout from './ClientLayout';
 import SeoMeta from '../components/SeoMeta';
+import Script from 'next/script';
 
 const barlow = Barlow({
   subsets: ['latin'],
@@ -51,7 +52,54 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="es" className={`${barlow.variable} ${barlowCondensed.variable} ${barlowSemiCondensed.variable}`}>
+      <head>
+        {/* Google Search Console - Meta Verification (Sustituye por tu código real si lo deseas) */}
+        <meta name="google-site-verification" content="TU_CODIGO_DE_VERIFICACION_DE_SEARCH_CONSOLE" />
+
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PHSFJ537');
+            `,
+          }}
+        />
+
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XZ9CSKPTFH"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XZ9CSKPTFH');
+            `,
+          }}
+        />
+      </head>
       <body className="font-sans antialiased text-slate-900 bg-slate-50">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PHSFJ537"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <SeoMeta path="/" schemaJson={defaultWebsiteSchema} />
         <ClientLayout>{children}</ClientLayout>
       </body>
